fbb973638527aca7923ebf0cbf849d54851dff0a
NikitaShakhovich <7149382@tut.by>
2022-03-01 23:23:21 +0300

    feat: add universal config handler

--- src/index.js
+++ src/index.js
@@ -1,46 +1,39 @@
-const open = ['(', '{', '[', '|', '1', '3', '5', '7', '8']
-const validationForm = {
- [')']: '(',
- ['}']: '{',
- [']']: '[',
- ['|']: '|',
- ['2']: '1',
- ['4']: '3',
- ['6']: '5',
- ['7']: '7',
- ['8']: '8',
-};
-
+const getConfig = (bracketsConfig) => {
+    let open = [];
+    let validationForm = {};
+    bracketsConfig.map(elem => {
+        open.push(elem[0]);
+        validationForm[elem[1]] = elem[0];
+    });
+    return {open: open, validationForm: validationForm,}
+}
 
 module.exports = function check(str, bracketsConfig) {
+
+ let configData = getConfig(bracketsConfig);
  let stack = [];
 
  for(let i = 0; i < str.length; i++) {
   let currentItem = str[i];
 
-  if(open.includes(currentItem)) {
-    if(currentItem === '7' && stack[stack.length - 1] === '7' || currentItem === '8' && stack[stack.length - 1] === '8' || currentItem === '|' && stack[stack.length - 1] === '|') {
+  if (configData.open.includes(currentItem)) {
+    if (currentItem === '7' && stack[stack.length - 1] === '7' || currentItem === '8' && stack[stack.length - 1] === '8' || currentItem === '|' && stack[stack.length - 1] === '|') {
        stack.pop();
        continue;
     }
-
     stack.push(currentItem);
-    console.log('Закинули в стек', stack);
   } else {
-    if(stack.length === 0) {
+    if (stack.length === 0) {
        return false;
     }
 
-   let topElement = stack[stack.length - 1];
-   console.log('topElement', topElement)
-    if (validationForm[currentItem] === topElement) {
-     stack.pop();
-   } else {
-     return false;
+    let topElement = stack[stack.length - 1];
+    if (configData.validationForm[currentItem] === topElement) {
+        stack.pop();
+    } else {
+        return false;
    }
   }
  }
-
- console.log('stack пустой - true', stack.length === 0)
  return stack.length === 0;
 }
